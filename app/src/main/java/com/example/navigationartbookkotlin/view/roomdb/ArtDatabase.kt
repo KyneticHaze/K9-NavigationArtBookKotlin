713f2975438a6ffe7a09aa3846083a5a9f478791
furkan harmancı <rocklee34@hotmail.com>
2023-09-18 14:08:04 +0300

    feat: database version is updated :sparkles:

--- app/src/main/java/com/example/navigationartbookkotlin/view/roomdb/ArtDatabase.kt
+++ app/src/main/java/com/example/navigationartbookkotlin/view/roomdb/ArtDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.navigationartbookkotlin.view.model.Art
 
-@Database(entities = [Art::class], version = 1)
+@Database(entities = [Art::class], version = 2)
 abstract class ArtDatabase : RoomDatabase() {
     abstract fun artDao() : ArtDao
 }
