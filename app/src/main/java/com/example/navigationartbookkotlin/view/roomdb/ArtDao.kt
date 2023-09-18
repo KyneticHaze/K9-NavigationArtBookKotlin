fdea40a45488c8c0434eb9b4963320044d7d3ddd
furkan harmancı <rocklee34@hotmail.com>
2023-09-18 14:08:41 +0300

    feat: rxjava comp is added :sparkles:

--- app/src/main/java/com/example/navigationartbookkotlin/view/roomdb/ArtDao.kt
+++ app/src/main/java/com/example/navigationartbookkotlin/view/roomdb/ArtDao.kt
@@ -2,16 +2,21 @@ package com.example.navigationartbookkotlin.view.roomdb
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Ignore
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.navigationartbookkotlin.view.model.Art
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
 
 @Dao
 interface ArtDao {
-    @Query("SELECT * FROM artTable")
-    fun getAll() : List<Art>
+    @Query("SELECT artName,id FROM Art")
+    fun getNameAndId() : Flowable<List<Art>>
+    @Query("SELECT * FROM Art WHERE id = :uid")
+    fun getId(uid : Int) : Flowable<Art>
     @Insert
-    fun insert(art: Art)
+    fun insert(art: Art) : Completable
     @Delete
-    fun delete(art: Art)
+    fun delete(art: Art) : Completable
 }
